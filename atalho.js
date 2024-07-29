bb073671bef740405011120774079cb1acaf7249
LeonardoBoni-018 <boni.leonardor@outlook.com>
2024-07-29 11:35:24 -0300

    Add files via upload

---
+++ atalho.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location.href = 'index.html'
+}
+
+function exatas(){
+    window.location.href = 'exatas.html'
+}
+
+function linguagens (){
+    window.location.href = 'linguagem.html'
+}
+
+function cinema (){
+    window.location.href = 'cinema.html'
+}
